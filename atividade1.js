c4f973cf4a33c529a6c0081bc10dac15e17dfffa
Luana Souza <87820972+LuanaSVieira@users.noreply.github.com>
2021-10-25 05:10:16 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,26 @@
+// 1)
+
+var dominio1 = "mundojs";
+var dominio2 = "google";
+
+console.log("Olá" + dominio1 + "!" + "O site www."+ dominio2 +".com te ajuda a achar muitos materiais de estudo.")
+
+// 2)
+
+var valorA = "casa";
+
+var valorB = "asa";
+
+valorA.toUpperCase;
+valorB.toUpperCase;
+
+var valorC = valorA == valorB;
+
+console.log(valorC);
+
+// 3)
+
+var valor1 = "Aprenda a utilizar.";
+valor1.substring(1, 17);
+
+console.log(valor1);
